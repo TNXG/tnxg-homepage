db26fb440a1e02e6e9e85549cc373b941dd3c858
TNXG <tnxg@outlook.jp>
2024-07-16 02:18:47 +0800

    chore: Update HTML attributes and meta tags for better SEO and accessibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 import { fileURLToPath } from 'url'
+import { SiteConfig } from '@/config';
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: SiteConfig.Language // 设置为中文
+      },
+      title: SiteConfig.title,
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: SiteConfig.description }
+      ],
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,7 +1,7 @@
 <template>
     <div :class="['avatar', { online: isOnline }, { offline: !isOnline }]">
         <div class="w-24 rounded-full">
-            <img src="https://api-space.tnxg.top/avatar?s=qq" />
+            <img src="https://api-space.tnxg.top/avatar?s=qq" alt="Active_Avatar" />
         </div>
     </div>
 </template>
